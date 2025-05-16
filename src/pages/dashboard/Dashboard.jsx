969d9177c2fd5b6761adbdb0cee8ea1f530ce2a2
Maxx2774 <maxviggonordin@hotmail.se>
2025-05-16 18:50:20 +0200

    ..

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import LinkedIn from "../../assets/icons/linkedin.svg?react";
+export default function Dashboard() {
+  return (
+    <main>
+      <a
+        href="https://www.linkedin.com/in/max-viggo-nordin-ab6b35254/"
+        target="_blank"
+      >
+        <LinkedIn />
+      </a>
+    </main>
+  );
+}
